e2557a49f7fad7788426d274070916561d22a23f
James Peret <james.peret@gmail.com>
2021-07-09 20:17:53 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var path = require("path");
+var fs = require("fs");
+
+class LotionTheme {
+    
+    constructor(server, config){
+        this.server = server;
+        this.config = config;
+        this.module_name = "Lotion Theme";
+    }
+
+    init()
+    {
+      var files = [
+        "node_modules/bootstrap/dist/css/bootstrap.min.css",
+        "node_modules/bootstrap/dist/css/bootstrap.bundle.min.js"
+      ]
+      for (let i = 0; i < files.length; i++) {
+        var _this = this;
+        
+        this.server.express.get("/" + files[i].split("/").pop(), function (req, res) {
+          var file = path.join(path.dirname(fs.realpathSync(__filename)), files[i]);
+          res.sendFile(file, function(err){
+            if (err) {
+              _this.server.cli.log("> Error: " + file + " - file not found");
+              res.status(err.status).end();
+            }
+            else {
+              _this.server.cli.log('> Sending ' + file +  _this.server.printUser(req));
+            }
+          });
+        });
+      }
+    }
+
+    run(){}
+      
+}
+
+module.exports = LotionTheme;
